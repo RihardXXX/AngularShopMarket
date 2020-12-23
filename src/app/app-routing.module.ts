558e497061cb543c1e5fceddc5d19aebbfb7f8c8
RihardXXX <yusupa.akaeva@yandex.ru>
2020-12-23 18:25:35 +0300

    create admin module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { CardPageComponent } from './card-page/card-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: MainPageComponent},
+      {path: 'product/:id', component: ProductPageComponent},
+      {path: 'cart', component: CardPageComponent}
+    ]
+  },
+  {
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.Adminmodule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
